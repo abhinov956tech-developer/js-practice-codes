05b5359d866ca6c49e71d57ad476f613487bd146
Abhinov Dutta <122796210+abhinov956tech-developer@users.noreply.github.com>
2024-07-04 17:45:27 +0000

    Commit done

---
+++ Practice-Jsbasics/02Variables.js
@@ -0,0 +1,7 @@
+const rollno ="03"
+let studentname="Abhinov"
+var gender="male"
+address="Doom Dooma"
+/*Prefer not to use var because of issue 
+in block scope and functional scope */
+console.table([rollno,studentname,gender,address])
---
+++ Practice-Jsbasics/03Datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version or newer js
+//alert(4+5)//we are using nodejs, not browser
+//code readability should be high
+let name ="Heyy"
+let age= 19
+let isloggedIn= false
+
+//number=>2 to power 53
+//bigint
+//string=> ""
+//boolean=> true/false
+//null => standalone value //typeof object
+//undefined => 
+//symbol=> unique
+
+//object
+
+console.log(typeof age);
