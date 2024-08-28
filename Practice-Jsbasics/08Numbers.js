cb166bc205034be10446d3294915f329fda98f72
Abhinov Dutta <122796210+abhinov956tech-developer@users.noreply.github.com>
2024-08-28 03:58:09 +0000

    Commit Done

---
+++ Practice-Jsbasics/08Numbers.js
@@ -0,0 +1,19 @@
+ const score =400
+ console.log(score);
+
+ const balance = new Number(500)
+ console.log(balance);
+
+ console.log(balance.toString().length);
+ console.log(balance.toFixed());
+
+ const otherNumber =23.8966
+
+ console.log(otherNumber.toPrecision(4));
+
+ const number=1000000
+ console.log(number.toLocaleString('en-In'));
+ 
+ 
+ 
+    
